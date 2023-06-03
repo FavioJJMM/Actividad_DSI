c8179b0e8b29c2450d04602d4aaff7b88a5511ea
FavioJM <125504081+FavioJM@users.noreply.github.com>
2023-06-02 23:25:18 -0600

    Editar tareas

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ function addTask(){
         checkbox.type='checkbox';
         const p=document.createElement('p');
         p.textContent=text;
+        p.setAttribute('contenteditable','false');
         
         const buttons=document.createElement('div');
         buttons.setAttribute('class','acciones');
         const editButton=document.createElement('button');
         editButton.textContent='Editar';
+        editButton.setAttribute('onclick','editTask(event)');
         const deleteButton=document.createElement('button');
         deleteButton.setAttribute('onclick','deleteTask(event)');
         deleteButton.textContent='Eliminar';
@@ -64,3 +66,20 @@ function clearlist(){
     const list=document.getElementById('completado');
     list.innerHTML='';
 }
+function editTask(event) {
+    const editButton = event.target;
+    const taskItem = editButton.parentNode.parentNode;
+    const paragraph = taskItem.querySelector('p');
+    
+    if (paragraph.getAttribute('contenteditable') === 'false') {
+      // Habilitar la edición del texto
+      paragraph.setAttribute('contenteditable', 'true');
+      paragraph.focus(); // Enfocar el elemento para facilitar la edición
+      editButton.textContent = 'Guardar'; // Cambiar texto del botón a "Guardar"
+    } else {
+      // Deshabilitar la edición del texto
+      paragraph.setAttribute('contenteditable', 'false');
+      editButton.textContent = 'Editar'; // Cambiar texto del botón a "Editar"
+    }
+  }
+  
